bfff86e2289b5e6a30ff8218a5708cc9f3a0eb7b
Ludovic Courtès <ludo@gnu.org>
2012-05-06 16:31:41 +0200

    Hack `%reader-standard-fault-handler' to read Unicode symbols on Guile 2.0.
    * modules/system/reader.scm: Switch to GPLv3+.
      (%reader-standard-fault-handler)[guile-2]: New procedure.
    
    * tests/scheme-reader.scm: Switch to GPLv3+.
      (scheme-reader): Pass `%reader-standard-fault-handler' as the second
      argument to `make-guile-reader'.

--- modules/system/reader.scm
+++ modules/system/reader.scm
@@ -7,7 +7,7 @@
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2 of the License, or
+;;; the Free Software Foundation; either version 3 of the License, or
 ;;; (at your option) any later version.
 ;;;
 ;;; This program is distributed in the hope that it will be useful,
@@ -59,6 +59,20 @@
 
 (load-extension "libguile-reader" "scm_reader_init_bindings")
 
+(cond-expand
+ (guile-2
+  (define (%reader-standard-fault-handler chr port reader)
+    ;; This hack allows non-ASCII Unicode chars to be correctly handled.  Of
+    ;; course, it doesn't help when the user defines their own handler (FIXME).
+    (if (and (char? chr) (char-set-contains? char-set:graphic chr))
+        (let ((sym (token-reader-procedure
+                    (standard-token-reader 'guile-symbol-misc-chars))))
+          (sym chr port reader))
+        (throw 'read-error "%reader-standard-fault-handler"
+               "unhandled character: ~S" (list chr)))))
+ (else
+  ;; Use `%reader-standard-fault-handler' as defined in `reader.c'.
+  #t))
 
 (load-extension "libguile-reader" "scm_load_standard_reader")
 
--- tests/scheme-reader.scm
+++ tests/scheme-reader.scm
@@ -13,7 +13,7 @@ exec ${GUILE-"${top_builddir-..}/pre-inst-guile"} -l $0  \
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2 of the License, or
+;;; the Free Software Foundation; either version 3 of the License, or
 ;;; (at your option) any later version.
 ;;;
 ;;; This program is distributed in the hope that it will be useful,
@@ -252,8 +252,11 @@ encountered."
 
 		  ;; Try out two versions of the Guile reader: one that
 		  ;; doesn't record positions and one that does.
+                  ;; Explicitly use `%reader-standard-fault-handler' so that
+                  ;; Unicode symbols are properly read on Guile 2.
 		  (list (default-reader)
-			(make-guile-reader #f 'reader/record-positions)))
+			(make-guile-reader %reader-standard-fault-handler
+                                           'reader/record-positions)))
 	(format #t "~%~%* summary~%~%total: ~a~%failed: ~a~%"
 		total failed))
 
